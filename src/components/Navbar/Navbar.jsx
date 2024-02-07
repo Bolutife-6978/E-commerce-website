b32dd21b58edcebdc203c64f686c380e824c89da
boluwatife <ogunfowokansherrfideen@gmail.com>
2024-02-07 22:29:58 +0100

    First commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const MenuLinks = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Shop",
+    link: "/#shop",
+  },
+  {
+    id: 3,
+    name: "About",
+    link: "/#about",
+  },
+  {
+    id: 3,
+    name: "Blogs",
+    link: "/#blog",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+      <div className="py-4">
+        <div className="container">
+          {/*Logo and Links Section */}
+          <div className="flex items-center gap-4">
+            <a
+              href="#"
+              className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
+            >
+              Eshop
+            </a>
+            {/* {Menu Items} */}
+            <div className="hidden lg:block">
+              <ul className="flex items-center gap-4">
+                {MenuLinks.map((data, index) => (
+                  <li key={index}>
+                    <a href={data.link}>{data.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          {/*Navbar Right Section */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
