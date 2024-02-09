04a65390dd2de9d84e479fa5d9c0f35ee121b5e9
boluwatife <ogunfowokansherrfideen@gmail.com>
2024-02-09 12:59:08 +0100

    Fixed DarkMode Issues

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -1,27 +1,45 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect } from "react";
 import LightButton from "../../assets/website/light-mode-button.png";
 import DarkButton from "../../assets/website/dark-mode-button.png";
 
 const DarkMode = () => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = React.useState(
+    localStorage.getItem("theme") || "dark"
+  );
+  // access to html element
+  const element = document.documentElement;
+
+  useEffect(() => {
+    // Update the class based on the theme
+    if (theme === "dark") {
+      element.classList.add("dark");
+      element.classList.remove("light");
+    } else {
+      element.classList.remove("dark");
+      element.classList.add("light");
+    }
+
+    // Save theme to local storage
+    localStorage.setItem("theme", theme);
+  }, [theme]); // Run this effect whenever 'theme' changes
+
   return (
     <div className="relative">
-      <img
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        src={LightButton}
-        alt=""
-        className={`w-12 
-        cursor-pointer  ${
-          theme === "dark" ? "opacity-0" : "opacity-100"
-        } transition-all duration-300`}
-      />
-      <img
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        src={DarkButton}
-        alt=""
-        className={"w-12 cursor-pointer"}
-      />
+      {theme === "dark" ? (
+        <img
+          onClick={() => setTheme("light")}
+          src={LightButton}
+          alt="Light Mode"
+          className="w-12 cursor-pointer transition-all duration-300"
+        />
+      ) : (
+        <img
+          onClick={() => setTheme("dark")}
+          src={DarkButton}
+          alt="Dark Mode"
+          className="w-12 cursor-pointer"
+        />
+      )}
     </div>
   );
 };
