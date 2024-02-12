8b7d8e3a3a646cd309d794a2d1dd5ea93d25da7d
boluwatife <ogunfowokansherrfideen@gmail.com>
2024-02-12 21:03:02 +0100

    Animated Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,20 @@ import Hero from "./components/Hero/Hero.jsx";
 import Category from "./components/Category/Category.jsx";
 import Category2 from "./components/Category/Category2.jsx";
 import Services from "./components/Services/Services.jsx";
+import Banner from "./components/Banner/Banner.jsx";
+import headphone from "./assets/hero/headphone.png"
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4:
+    "Enjoy our winter sales for out gadgets at discount prices that won't hit hard on your pockets",
+  bgColor: "#f42c37",
+};
 
 const App = () => {
   return (
@@ -13,6 +27,7 @@ const App = () => {
       <Category />
       <Category2 />
       <Services />
+      <Banner data = {BannerData} />
     </div>
   );
 };
